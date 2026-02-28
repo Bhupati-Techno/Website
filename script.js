96e20a5c0a387ecbb84eb6c22635f4af0d227cce
kashwin007-pixel <k.ashwin007@gmail.com>
2026-03-01 02:17:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    document.getElementById("loader").style.display = "none";
+};
+
+particlesJS("particles-js", {
+    particles: {
+        number: { value: 70 },
+        color: { value: "#2563eb" },
+        size: { value: 3 },
+        move: { speed: 2 }
+    }
+});
